a5ad3d417f0420814bb2f51c951494dacd6ecbac
Joel Pick <joel.l.pick@gmail.com>
2017-10-21 16:31:45 +1100

    updated point remove in scatter plot

--- R/S_extract.R
+++ R/S_extract.R
@@ -23,30 +23,11 @@ single_scatter_extract <- function(image, calpoints, point_vals){
 			raw_data <- rbind(raw_data, data.frame(x=select_points$x,y=select_points$y))
 		}
 		if(add_removeQ=="r") {
-			cat("Click on point you want to remove\n")
-			remove <- locator(1,type="p", col="green", pch=4, lwd=2)
-			x_remove <- which(abs(raw_data$x-remove$x) == min(abs(raw_data$x-remove$x)))
-			y_remove <- which(abs(raw_data$y-remove$y) == min(abs(raw_data$y-remove$y)))
-			## add in a maximum distance to search in; at the moment it searches whole plot area
-			#
-
-			if(length(x_remove)>1 & length(y_remove)>1){
-				cat("**** Point not identified ****\n")
-			}else if(length(x_remove)>1 & length(y_remove)==1){
-				x_remove <- x_remove[x_remove %in% y_remove]
-				raw_data <- raw_data[-x_remove,]
+			cat("Click on points you want to remove\n")
+			remove <- identify(raw_data, ,offset=0,labels="*", cex=2, col="green")
+			if(length(remove)>0) {
+				raw_data <- raw_data[-remove,]
 				internal_redraw(image, plot_type="scatterplot", calpoints, point_vals, raw_data)
-				cat("**** Point successfully removed ****\n")
-			}else if(length(y_remove)>1 & length(x_remove)==1){
-				y_remove <- y_remove[y_remove %in% x_remove]
-				raw_data <- raw_data[-x_remove,]
-				internal_redraw(image, plot_type="scatterplot", calpoints, point_vals, raw_data)
-				cat("**** Point successfully removed ****\n")
-			}else if(x_remove==y_remove) {
-				raw_data <- raw_data[-x_remove,]
-				internal_redraw(image, plot_type="scatterplot", calpoints, point_vals, raw_data)
-				cat("**** Point successfully removed ****\n")	
-			}else{cat("**** Point not identified ****\n")
 			}
 		}
 		add_removeQ <- readline("Add, remove or continue? a/r/c ")		
