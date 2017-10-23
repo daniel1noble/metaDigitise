e421339507593fc4dfc1b98af3d98dfe3a9b8e2b
Daniel Noble <daniel.wa.noble@gmail.com>
2017-10-24 07:38:49 +1100

    Implemented a summary argument in bulk_digitise, but not yet tested. Also, prints new error message if all files have been done within a directory already

--- R/bulk_metaDigitise.R
+++ R/bulk_metaDigitise.R
@@ -2,16 +2,17 @@
 #' @description Batch processes png files within a set directory, consolidates the data and exports the data for each image and type
 #' @param dir the path name to the directory / folder where the files are located
 #' @param types Argument specifying whether the types of images are the same (i.e., all scatter plots - "same") or a mixture of different plot types (i.e., scatter plots, means and se - "diff").
+#' @param ... Other arguments called from metaDigitise. summary = TRUE or FALSE is most relevant as it will print a simple summary statistics that are the same across all files.
 #' @return If type = "same" the function returns a dataframe with the relevant data for each figure being digitised. If type = "diff" it returns a list of the relevant data.
 #' @export
 
-bulk_metaDigitise <- function(dir, types = c("diff", "same")) {
+bulk_metaDigitise <- function(dir, types = c("diff", "same"), ...) {
 	
 	   type <- match.arg(types)
 			   setup_calibration_dir(dir)
 	details <- get_notDone_file_details(dir)
 
-	 plot_type <-  if (type == "diff") {NULL} else{specify_type()}
+	 plot_type <-  if (type == "diff") {NULL} else{ specify_type() }
 		 
 		 data_list <- list()
 
@@ -24,11 +25,11 @@ bulk_metaDigitise <- function(dir, types = c("diff", "same")) {
 
 	if (type == "diff") {
 		
-		return(extract_digitised(data_list))
+		return(extract_digitised(data_list, ...))
 
 	} else{
 		
-		return(do.call(rbind, extract_digitised(data_list)))
+		return(do.call(rbind, extract_digitised(data_list, ...)))
 	}
 }
 
@@ -50,12 +51,18 @@ specify_type <- function(){
 
 #' @title extract_digitised
 #' @param list A list of objects returned from metaDigitise
+#' @param summary A logical 'TRUE' or 'FALSE' indicating whether metaDigitise should print summary statitics from each figure and group.
 #' @description Function for extracting the data from a metaDigitise list
 #' @return The function will return a data frame with the data across all the digitised files 
 #' @export
 
-extract_digitised <- function(list) {
-	return( do.call (rbind, lapply (list, function(x) x$processed_data) ))
+extract_digitised <- function(list, summary = FALSE) {
+
+	if(summary == TRUE) {
+		return( do.call (rbind, lapply (list, function(x) summary(x)) ))		
+	} else{
+		return( do.call (rbind, lapply (list, function(x) x$processed_data) ))
+	}
 }
 
 #' @title setup_calibration_dir
@@ -81,6 +88,12 @@ get_notDone_file_details <- function(dir){
 	       paths <- paste0(dir, images)
 	     cal_dir <- paste0(dir, "caldat/")
 	calibrations <- list.files(paste0(dir, "caldat/"))
+
+	# Check whether there are new files still needing to be done.
+
+	if(length(calibrations) == length(name)) {
+		stop("Congratulations! Looks like you have finished digitising all figures in this directory. If you haven't please delete files from the caldat/ folder and try again!", call. = FALSE)
+	}
 
 	# Find what files are already done. Remove these from our list
 	if (length(calibrations) > 1){
