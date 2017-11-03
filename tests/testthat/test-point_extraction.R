bbc0e67b8574752add4ebde918185d9b5c48e4ef
Joel Pick <joel.l.pick@gmail.com>
2017-11-03 17:11:07 +1100

    point_extraction testing

---
+++ tests/testthat/test-point_extraction.R
@@ -0,0 +1,29 @@
+context("Check point_extraction...")
+
+
+point_extract_tester_func <- function(...) {
+	with_mock(
+		MB_extract = function(...) 1,
+		group_scatter_extract = function(...) 2,
+		histogram_extract = function(...) 3,
+		point_extraction (...)
+		)
+}
+
+test_that("Checking point_extraction..", {
+	expect_equal(
+ 		point_extract_tester_func(object=list(plot_type="mean_error")), 
+		1,
+		info = "user_options failed")
+
+	expect_equal(
+ 		point_extract_tester_func(object=list(plot_type="scatterplot")), 
+		2,
+		info = "user_options failed")
+
+	expect_equal(
+ 		point_extract_tester_func(object=list(plot_type="histogram")), 
+		3,
+		info = "user_options failed")
+})
+ 
