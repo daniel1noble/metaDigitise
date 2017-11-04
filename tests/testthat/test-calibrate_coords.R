16430fe4d1fccd9da31f1b34bc4ead46eb55b91b
Joel Pick <joel.l.pick@gmail.com>
2017-11-04 21:21:18 +1100

    more testing

--- tests/testthat/test-calibrate_coords.R
+++ tests/testthat/test-calibrate_coords.R
@@ -21,4 +21,45 @@ test_that("Checking cal_coords..", {
 		)
 	)
 })
+
+
 	
+test_that("Checking getVals..", {
+		
+	with_mock(
+		`metaDigitise::user_numeric` = mockery::mock(1,2,3,4),
+		expect_equal(
+			getVals(calpoints=data.frame(x=c(0,0,0,100),y=c(0,100,0,0))), 
+			c(y1=1,y2=2,x1=3,x2=4), 
+			info = "getVals failed"
+		)
+	)
+
+	with_mock(
+		`metaDigitise::user_numeric` = mockery::mock(1,2,3,4),
+		expect_equal(
+			getVals(calpoints=data.frame(x=c(0,0),y=c(0,100))), 
+			c(y1=1,y2=2), 
+			info = "getVals failed"
+		)
+	)
+})
+
+
+
+test_that("Checking getVals..", {
+	with_mock(
+		`metaDigitise::internal_redraw` = function(...){},
+		`metaDigitise::print_cal_instructions` = function(...){},
+		`metaDigitise::cal_coords` = function(...) data.frame(x=c(0,0,0,100),y=c(0,100,0,0)),
+		`metaDigitise::getVals` = function(...) c(y1=1,y2=2,x1=3,x2=4),
+		readline = function(...) "n",
+		expect_equal(
+			user_calibrate(object=list()),
+			list(calpoints=data.frame(x=c(0,0,0,100),y=c(0,100,0,0)), point_vals=c(y1=1,y2=2,x1=3,x2=4)), 
+			info = "getVals failed"
+		)
+	)
+
+})
+
---
+++ tests/testthat/test-metaDigitise_functions.R
@@ -0,0 +1,27 @@
+
+context("Check metaDigitise functions...")
+
+
+mock_object <- list(
+	image_file = "./image.png",
+	flip=FALSE,
+	rotate=0,
+	plot_type="mean_error",
+	variable="y",
+	calpoints = data.frame(x=c(0,0),y=c(0,100)), 
+	point_vals = c(1,2), 
+	entered_N=TRUE,
+	raw_data = data.frame(id=rep("control",2), x=c(60,60), y=c(75,50), n=rep(20,2)),
+	error_type="sd",
+	processed_data=data.frame(id=as.factor("control"),mean=1.5, error=0.25, n=20, variable="y", stringsAsFactors = FALSE)	
+	)
+
+class(mock_object) <- 'metaDigitise'
+
+test_that("Checking summary.metaDigitise..", {
+	expect_equal(
+		summary.metaDigitise(mock_object),
+		data.frame(
+			filename=as.factor("image.png"), group_id=as.factor("control"), variable=as.factor("y"), mean=1.5, sd=0.25, n=20, r=NA, plot_type="mean_error", stringsAsFactors = FALSE)
+	)
+})
