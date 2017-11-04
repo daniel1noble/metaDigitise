8f8b38b432e5a74cb2f2b1215cea19047c0bd14b
Joel Pick <joel.l.pick@gmail.com>
2017-11-04 20:20:19 +1100

    fixing testing bugs

--- tests/testthat/test-calibrate_coords.R
+++ tests/testthat/test-calibrate_coords.R
@@ -4,7 +4,7 @@ context("Check calibrate_coords...")
 test_that("Checking cal_coords..", {
 		
 	with_mock(
-		locator = mock(list(x=c(0,0),y=c(0,100)), list(x=c(0,100),y=c(0,0))),
+		locator = mockery::mock(list(x=c(0,0),y=c(0,100)), list(x=c(0,100),y=c(0,0))),
 		expect_equal(
 			cal_coords(plot_type="scatterplot"), 
 			data.frame(x=c(0,0,0,100),y=c(0,100,0,0)), 
@@ -13,7 +13,7 @@ test_that("Checking cal_coords..", {
 	)
 
 	with_mock(
-		locator = mock(list(x=c(0,0),y=c(0,100)), list(x=c(0,100),y=c(0,0))),
+		locator = mockery::mock(list(x=c(0,0),y=c(0,100)), list(x=c(0,100),y=c(0,0))),
 		expect_equal(
 			cal_coords(plot_type="mean_error"), 
 			data.frame(x=c(0,0),y=c(0,100)), 
--- tests/testthat/test-point_extraction.R
+++ tests/testthat/test-point_extraction.R
@@ -1,29 +1,32 @@
 context("Check point_extraction...")
 
 
-point_extraction_tester_func <- function(...) {
+
+
+test_that("Checking point_extraction..", {
+
+point_extraction_tester_func <- function(object,...) {
 	with_mock(
-		MB_extract = function(...) 1,
-		group_scatter_extract = function(...) 2,
-		histogram_extract = function(...) 3,
-		point_extraction (...)
+		`metaDigitise::MB_extract` = function(...) 1,
+		`metaDigitise::group_scatter_extract` = function(...) 2,
+		`metaDigitise::histogram_extract` = function(...) 3,
+		point_extraction (object)
 		)
 }
 
-test_that("Checking point_extraction..", {
 	expect_equal(
  		point_extraction_tester_func(object=list(plot_type="mean_error")), 
 		1,
-		info = "user_options failed")
+		info = "point_extraction failed")
 
 	expect_equal(
  		point_extraction_tester_func(object=list(plot_type="scatterplot")), 
 		2,
-		info = "user_options failed")
+		info = "point_extraction failed")
 
 	expect_equal(
  		point_extraction_tester_func(object=list(plot_type="histogram")), 
 		3,
-		info = "user_options failed")
+		info = "point_extraction failed")
 })
  
