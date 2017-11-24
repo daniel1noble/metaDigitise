fc71cfa33bd797a510be2f03442f50f481f56ef1
Daniel Noble <daniel.wa.noble@gmail.com>
2017-11-24 14:11:51 +1100

    Added metaDigitise example that can be run

--- R/metaDigitise.R
+++ R/metaDigitise.R
@@ -15,6 +15,32 @@
 #' # data <- metaDigitise(dir = "./example_figs/", summary = TRUE)
 #' # data
 #' @return A data frame or list containing the raw digitised data or the processed, summary statistics from the digitised data
+#' @examples
+#' \dontrun{# Generate a directory in current working directory and fill with figures
+#' dir.create("./prac_figs")
+#' set.seed(103)
+#' x <- rnorm(20,0,1)
+#' y <- rnorm(20,0,1)
+#' means <- c(mean(x),mean(y))
+#' ses <- c(sd(x)/sqrt(length(x))*1.96, sd(y)/sqrt(length(y))*1.96)
+#' png(filename = paste0("./prac_figs/mean_error.png"), width = 480, height = 480)
+#' plot(means, ylim = c(min(means-ses)-0.1,max(means+ses)+0.1), xlim=c(0.5,2.5), 
+#' xaxt="n", pch=19, cex=2, ylab="Variable +/- SE", xlab="Treatment", main="Mean Error")
+#' arrows(1:length(means),means+ses, 1:length(means), means-ses, code=3, angle=90, length=0.1)
+#' axis(1,1:length(means),names(means))
+#' dev.off()
+#' png(filename = paste0("boxplot.png"), width = 480, height = 480)
+#' boxplot(x,y, main="Boxplot")
+#' dev.off()
+#' png(filename = paste0("histogram.png"),width = 480, height = 480)
+#' hist(c(x,y), xlab= "variable", main="Histogram")
+#' dev.off()
+#' png(filename = paste0("scatterplot.png"), width = 480, height = 480)
+#' plot(x,y, main="Scatterplot")
+#' dev.off()
+#' 
+#' data <- metaDigitise("./prac_figs/")
+#' }
 #' @export
 
 metaDigitise<-function(dir, summary = TRUE){
