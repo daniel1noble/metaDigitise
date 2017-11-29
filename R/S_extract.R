77ca9d2bca9f8785e6f89418cf4b8f24f7904965
Joel Pick <joel.l.pick@gmail.com>
2017-11-29 13:56:07 +1100

    scatterplot update

--- R/S_extract.R
+++ R/S_extract.R
@@ -15,13 +15,18 @@ delete_group <- function(raw_data){
 #' @title edit_group
 #' @param raw_data data
 #' @param group_id group_id
+#' @param calpoints The calibration points
 #' @param ... other functions to pass to internal_redraw
 #' @description Edit group points in scatterplots
  
-edit_group <- function(raw_data, group_id,...){
+edit_group <- function(raw_data, group_id, calpoints, ...){
 	
 	cols <- rep(c("red", "green", "purple"),length.out=90)
 	pchs <- rep(rep(c(19, 17, 15),each=3),length.out=90)
+
+	box_y <- c(mean(calpoints$y[3:4]), mean(calpoints$y[3:4]),0,0,mean(calpoints$y[3:4]))/2
+	box_x <- c(0,mean(calpoints$x[1:2]), mean(calpoints$x[1:2]),0,0)/2
+
 
 	if(!is.null(group_id)) {
 		group_data <- data.frame()
@@ -44,22 +49,30 @@ edit_group <- function(raw_data, group_id,...){
 
 	while(add_removeQ!="c"){
 
-		if(add_removeQ=="a"){		
-			cat("\nClick on points you want to add.\nIf you want to remove a point, or are finished with a\ngroup, exit (see above), then follow prompts\n")
-			select_points <- locator(type="p", lwd=2, col=cols[i], pch=pchs[i])
-			if(!is.null(select_points)){
+		if(add_removeQ=="a"){
+			polygon(box_x,box_y, col="red", border=NA)
+			cat("\nClick on points you want to add.\nIf you want to remove a point, or are finished with a group, \nexit by clicking on red box in bottom left corner, then follow prompts\n")
+		} 
+		while(add_removeQ=="a"){
+			select_points <- locator(1,type="p", lwd=2, col=cols[i], pch=pchs[i])			
+			if( select_points$x<max(box_x) & select_points$y<max(box_y) & select_points$x>min(box_x) & select_points$y>min(box_y)) {
+				add_removeQ <- "b"
+			}
+			else{ 
 				group_data <- rbind(group_data, data.frame(id=group_id, x=select_points$x, y=select_points$y, group=i, col=cols[i], pch=pchs[i]) )
-			}		
+			}
 		}
 
 		if(add_removeQ=="d"){
-			cat("\nClick on points you want to delete\nOnce you are finished removing points exit (see above)\n")
-			remove <- identify(group_data$x,group_data$y ,offset=0,labels="*", cex=2, col="green")
-			if(length(remove)>0) 
-			group_data <- group_data[-remove,]
+			cat("\nClick on point you want to delete\n")
+			remove <- identify(group_data$x,group_data$y, n=1)
+			if(length(remove)>0) {
+				points(group_data$x[remove], group_data$y[remove],cex=2, col="white", pch=19)
+				group_data <- group_data[-remove,]
+			}
 		}
 
-		internal_redraw(...,raw_data=rbind(raw_data, group_data), calibration=TRUE, points=TRUE)
+		internal_redraw(...,calpoints=calpoints,raw_data=rbind(raw_data, group_data), calibration=TRUE, points=TRUE)
 		add_removeQ <- readline("\nAdd or Delete points to this group, or Continue? (a/d/c) \n")
 	}
 
@@ -76,14 +89,6 @@ edit_group <- function(raw_data, group_id,...){
 
 group_scatter_extract <- function(edit=FALSE, raw_data = data.frame(), ...){
 
-	cat(
-    #"..............NOW .............",
-    "\nFollow instructions below, to exit point adding or removing:",
-    " - Windows: right click on the plot area and choose 'Stop'!",
-    " - X11: hit any mouse button other than the left one.",
-    " - quartz/OS X: hit ESC\n",
-    sep = "\n\n")
-
 	editQ <- if(edit){ "b" }else{ "a" }
 	if(!edit) cat("\nIf there are multiple groups, enter unique group identifiers (otherwise press enter)")
 
@@ -92,15 +97,11 @@ group_scatter_extract <- function(edit=FALSE, raw_data = data.frame(), ...){
 		group_id <- NULL
 
 		if(editQ=="a"){
-			# if(nrow(raw_data)==0){
-			# 	group_id <- readline("\nGroup identifier: ")
-			# }else{
 			group_id <- user_unique("\nGroup identifier: ", unique(raw_data$id))
-			# }
 			editQ <- "e"
 		}
 
-		if(editQ == "e") raw_data <- edit_group(raw_data, group_id,...)
+		if(editQ == "e") raw_data <- edit_group(raw_data, group_id, ...)
 
 		if(editQ == "d") raw_data <- delete_group(raw_data)
 	
