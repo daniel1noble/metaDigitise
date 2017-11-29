b48632dd5659fbf411c9bae8029b8eed265275d9
Joel Pick <joel.l.pick@gmail.com>
2017-11-29 12:20:40 +1100

    fixed bug

--- R/H_extract.R
+++ R/H_extract.R
@@ -1,9 +1,10 @@
 #' @title histogram_extract
 #' @param edit logical; whether in edit mode 
 #' @param raw_data raw data
+#' @param calpoints The calibration points
 #' @param ... arguments to pass to internal_redraw
 #' @description Extraction of data from histograms
-histogram_extract <- function(edit=FALSE, raw_data = data.frame(), ...){
+histogram_extract <- function(edit=FALSE, raw_data = data.frame(), calpoints, ...){
 
 	if(edit){ 
 		idQ <- user_options("Change group identifier? (y/n) ",c("y","n"))
@@ -20,7 +21,10 @@ histogram_extract <- function(edit=FALSE, raw_data = data.frame(), ...){
 	histQ <- if(edit){ "b" }else{ "a" }
 	i <- if(edit){ max(unique(raw_data$bar)) }else{ 0 }
 	bar_cols <- c("red","orange")
-	
+	box_y <- c(mean(calpoints$y[3:4]), mean(calpoints$y[3:4]),0,0,mean(calpoints$y[3:4]))/2
+	box_x <- c(0,mean(calpoints$x[1:2]), mean(calpoints$x[1:2]),0,0)/2
+
+
 	while(histQ != "f"){
 		
 		if(histQ=="a"){
@@ -30,7 +34,10 @@ histogram_extract <- function(edit=FALSE, raw_data = data.frame(), ...){
 		while(histQ=="a"){
 			i=i+1
 			bar_points <- locator(2, type="o", col=bar_cols[is.even(i)+1], lwd=2, pch=19)
-			if( mean(bar_points$x)<max(box_x) & mean(bar_points$y)<max(box_y) & mean(bar_points$x)>min(box_x) & mean(bar_points$y)>min(box_y)) histQ <- "b"
+			if( mean(bar_points$x)<max(box_x) & mean(bar_points$y)<max(box_y) & mean(bar_points$x)>min(box_x) & mean(bar_points$y)>min(box_y)){
+					histQ <- "b"
+					i=i-1
+				}
 			if(histQ=="a") raw_data <- rbind(raw_data, data.frame(id=group_id, x=bar_points$x,y=bar_points$y, bar=i))
 		}
 
@@ -39,7 +46,7 @@ histogram_extract <- function(edit=FALSE, raw_data = data.frame(), ...){
 			raw_data <- subset(raw_data, bar != delQ)
 		}
 
-		internal_redraw(raw_data=raw_data,...)
+		internal_redraw(raw_data=raw_data, calpoints=calpoints, ...)
 		histQ <- readline("Add bar, Delete bar or Finish plot? (a/d/f) \n")
 
 	}
