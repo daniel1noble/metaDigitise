777dd38101993c93e50496b0df384dac08227a7b
Daniel Noble <daniel.wa.noble@gmail.com>
2018-06-12 11:07:14 +1000

    Fixing CRAN comments

--- R/metaDigitise.R
+++ R/metaDigitise.R
@@ -13,30 +13,31 @@
 #' @author Daniel Noble - daniel.wa.noble@gmail.com
 #' @return A data frame or list containing the raw digitised data or the processed, summary statistics from the digitised data
 #' @examples
-#' \dontrun{# Generate a directory in current working directory and fill with figures
-#' dir.create("./prac_figs")
+#' \dontrun{
+#' # temporary directory
+#' tmp_dir <- tempdir()
 #' set.seed(103)
 #' x <- rnorm(20,0,1)
 #' y <- rnorm(20,0,1)
 #' means <- c(mean(x),mean(y))
 #' ses <- c(sd(x)/sqrt(length(x))*1.96, sd(y)/sqrt(length(y))*1.96)
-#' png(filename = paste0("./prac_figs/mean_error.png"), width = 480, height = 480)
+#' png(filename = paste0(tmp_dir,"/mean_error.png"), width = 480, height = 480)
 #' plot(means, ylim = c(min(means-ses)-0.1,max(means+ses)+0.1), xlim=c(0.5,2.5), 
 #' xaxt="n", pch=19, cex=2, ylab="Variable +/- SE", xlab="Treatment", main="Mean Error")
 #' arrows(1:length(means),means+ses, 1:length(means), means-ses, code=3, angle=90, length=0.1)
 #' axis(1,1:length(means),names(means))
 #' dev.off()
-#' png(filename = paste0("./prac_figs/boxplot.png"), width = 480, height = 480)
+#' png(filename = paste0(tmp_dir, "/boxplot.png"), width = 480, height = 480)
 #' boxplot(x,y, main="Boxplot")
 #' dev.off()
-#' png(filename = paste0("./prac_figs/histogram.png"),width = 480, height = 480)
+#' png(filename = paste0(tmp_dir, "/histogram.png"),width = 480, height = 480)
 #' hist(c(x,y), xlab= "variable", main="Histogram")
 #' dev.off()
-#' png(filename = paste0("./prac_figs/scatterplot.png"), width = 480, height = 480)
+#' png(filename = paste0(tmp_dir, "/scatterplot.png"), width = 480, height = 480)
 #' plot(x,y, main="Scatterplot")
 #' dev.off()
 #' 
-#' data <- metaDigitise("./prac_figs/")
+#' data <- metaDigitise(tmp_dir)
 #' }
 #' @export
 
