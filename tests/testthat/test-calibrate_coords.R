6ff8a38bef16bd79da5cec165f2f371ba655cc85
Daniel Noble <daniel.wa.noble@gmail.com>
2020-03-12 22:09:28 +1100

    Updating tests to avoid errors in new R dev

--- tests/testthat/test-calibrate_coords.R
+++ tests/testthat/test-calibrate_coords.R
@@ -57,7 +57,7 @@ testthat::test_that("Checking user_calibrate..", {
 		readline = function(...) "n",
 		testthat::expect_equal(
 			user_calibrate(object=list(plot_type="scatterplot")),
-			list(calpoints=data.frame(x=c(0,0,0,100),y=c(0,100,0,0)), point_vals=c(y1=1,y2=2,x1=3,x2=4), log_axes=c(axes="n")), 
+			list(calpoints=data.frame(x=c(0,0,0,100),y=c(0,100,0,0), stringsAsFactors = TRUE), point_vals=c(y1=1,y2=2,x1=3,x2=4), log_axes=c(axes="n")), 
 			info = "user_calibrate failed"
 		)
 	)
